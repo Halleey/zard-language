b63369c9c8aab3919d23b68fd9abbd1523f5d6d8
hallyson <aparecidohallyson@gmail.com>
2024-08-10 23:33:20 -0300

    fixed bugs

--- src/WhileStatement.java
+++ src/WhileStatement.java
@@ -6,32 +6,28 @@ public class WhileStatement {
     }
 
     public void execute() {
-        parser.eat(Token.TokenType.KEYWORD); // Consume 'while'
-        parser.eat(Token.TokenType.DELIMITER); // Consume '('
+        parser.eat(Token.TokenType.KEYWORD);
+        parser.eat(Token.TokenType.DELIMITER);
 
-        // Avalia a condição
         Object condition = parser.expression();
         if (!(condition instanceof Boolean)) {
             throw new RuntimeException("Condição de 'while' deve ser um valor booleano.");
         }
 
-        parser.eat(Token.TokenType.DELIMITER); // Consume ')'
-        parser.eat(Token.TokenType.DELIMITER); // Consume '{'
+        parser.eat(Token.TokenType.DELIMITER);
+        parser.eat(Token.TokenType.DELIMITER);
+
 
         while ((boolean) condition) {
             processWhileBlock();
-
-            // Reavalie a condição
             condition = checkWhileCondition();
-            if (!(boolean) condition) {
-                // Pular até o final do bloco da função
-                skipToEndOfFunction();
+            if (!(condition instanceof Boolean) || !(boolean) condition) {
                 break;
             }
         }
 
-        // Avançar para o próximo token após o final do bloco do while
         if (parser.getCurrentToken().getType() != Token.TokenType.EOF) {
+            skipToEndOfFunction();
             parser.advance();
         }
     }
@@ -52,22 +48,27 @@ public class WhileStatement {
                 throw new RuntimeException("Erro de sintaxe: bloco não terminado");
             }
         }
+        parser.eat(Token.TokenType.DELIMITER);
     }
 
     private Object checkWhileCondition() {
         parser.backToWhile();
-        parser.eat(Token.TokenType.KEYWORD); // Consume 'while'
-        parser.eat(Token.TokenType.DELIMITER); // Consume '('
+        parser.eat(Token.TokenType.KEYWORD);
+        parser.eat(Token.TokenType.DELIMITER);
 
         Object condition = parser.expression();
-        parser.eat(Token.TokenType.DELIMITER); // Consume ')'
-        parser.eat(Token.TokenType.DELIMITER); // Consume '{'
+        if (!(condition instanceof Boolean)) {
+            throw new RuntimeException("Condição de 'while' deve ser um valor booleano.");
+        }
+
+        parser.eat(Token.TokenType.DELIMITER);
+        parser.eat(Token.TokenType.DELIMITER); // Consome '{'
         return condition;
     }
 
     private void skipToEndOfFunction() {
         int braceLevel = 0;
-        while (!(parser.getCurrentToken().getType() == Token.TokenType.EOF)) {
+        while (parser.getCurrentToken().getType() != Token.TokenType.EOF) {
             if (parser.getCurrentToken().getType() == Token.TokenType.DELIMITER) {
                 if ("{".equals(parser.getCurrentToken().getValue())) {
                     braceLevel++;
